18a6a6405ffd5614829837dd58fa1b74f3f3e675
Adrien Dimitri <adrien.maugueret@proton.me>
2025-06-22 20:44:13 +0200

    improved staticbase simulator

--- envs/JSBSim/core/simulatior.py
+++ envs/JSBSim/core/simulatior.py
@@ -535,51 +535,77 @@ class MissileSimulator(BaseSimulator):
 
 
 class StaticSimulator(BaseSimulator):
-    def __init__(self, uid, color, model, type, init_state=None, origin=(120.5, 60.5, 0.0)):
+    def __init__(self, uid, color, model, type, active=False, init_state=None, origin=(120.5, 60.5, 0.0)):
         super().__init__(uid, color, dt=0)
         self.model = model
         self.type = type
         self.origin = origin
         self.rendered = False
-        # Set position from init_state if provided
+        self.active = active
+
+        self.lon0, self.lat0, self.alt0 = origin
+
+        # Default position
+        lon, lat, alt = origin
         if init_state is not None:
-            lon = init_state.get('enemy_base/longitude-geod-deg', origin[0])
-            lat = init_state.get('enemy_base/latitude-geod-deg', origin[1])
-            alt = init_state.get('enemy_base/altitude-ft', origin[2]) * 0.3048  # ft to m
-            self._position = np.array([lon, lat, alt])
-        else:
-            self._position = np.zeros(3)
-    
+            lon = init_state.get('enemy_base/longitude-geod-deg', lon)
+            lat = init_state.get('enemy_base/latitude-geod-deg', lat)
+            alt = init_state.get('enemy_base/altitude-ft', alt) * 0.3048  # ft to m
+            active = init_state.get('enemy_base/active', active)
+
+        self._geodetic = np.array([lon, lat, alt])
+        self._position = LLA2NEU(lon, lat, alt, self.lon0, self.lat0, self.alt0)
+        self.active = active
+
+
     def get_property_value(self, prop):
-        """Get the value of the specified property for static objects."""
         if isinstance(prop, Property):
-            # Use the actual property names as defined in your Catalog
             if prop.name_jsbsim == "enemy_base/longitude-geod-deg":
-                return self._position[0]
+                return self._geodetic[0]
             elif prop.name_jsbsim == "enemy_base/latitude-geod-deg":
-                return self._position[1]
+                return self._geodetic[1]
             elif prop.name_jsbsim == "enemy_base/altitude-ft":
-                return self._position[2] / 0.3048
+                return self._geodetic[2] / 0.3048
             elif prop.name_jsbsim == "enemy_base/altitude-m":
-                return self._position[2]
+                return self._geodetic[2]
             else:
                 raise ValueError(f"Property {prop.name_jsbsim} not supported by StaticSimulator.")
         else:
             raise ValueError(f"prop type unhandled: {type(prop)} ({prop})")
 
+    @property
+    def is_active(self):
+        return self.active
+
+    @is_active.setter
+    def is_active(self, value: bool):
+        self.active = value
+
     def run(self, **kwargs):
         pass
+
     def close(self):
         pass
-    def log(self):
-        if not self.rendered:
-            self.rendered = True
-            # Render the static base to the ACMI file
-            pos = self._position
-            print(pos)
-            return f"{self.uid},T={pos[0]}|{pos[1]}|{pos[2]}|0|0|0,Length=1000.0,Width=1000.0,Height=1000.0,Name={self.model},Color={self.color},Type={self.type}"
-        return ""
 
+    def log(self):
+        lon, lat, alt = self._geodetic
+        if self.active:
+            if not self.rendered:
+                self.rendered = True
+                return (
+                    f"{self.uid},T={lon}|{lat}|{alt}|0|0|0,"
+                    f"Length=1000.0,Width=1000.0,Height=1000.0,"
+                    f"Name={self.model},Color={self.color},Type={self.type},Visible=1"
+                )
+        else:
+            if self.rendered:
+                self.rendered = False  # Allow re-rendering again if re-activated
+                return (
+                    f"{self.uid},T={lon}|{lat}|{alt}|0|0|0,"
+                    f"Length=1000.0,Width=1000.0,Height=1000.0,"
+                    f"Name={self.model},Color={self.color},Type={self.type},Visible=0"
+                )
+        return ""
 
 class WaypointSimulator(BaseSimulator):
     def __init__(self, uid, color, model, type, active, init_state=None, origin=(120.0, 60.0, 0.0)):
