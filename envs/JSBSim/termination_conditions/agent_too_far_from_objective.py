a15036136bc7c11186d7950dbe60cab1eb54db13
Adrien Dimitri <adrien.maugueret@proton.me>
2025-06-22 12:43:33 +0200

    removed crash on done

--- envs/JSBSim/termination_conditions/agent_too_far_from_objective.py
+++ envs/JSBSim/termination_conditions/agent_too_far_from_objective.py
@@ -9,8 +9,8 @@ class AgentTooFar(BaseTerminationCondition):
 
     def __init__(self, config):
         super().__init__(config)
-        self.max_distance = getattr(config, 'max_waypoint_distance', 20000.0)  # meters (20 km default)
-
+        self.max_distance = getattr(config, 'max_waypoint_distance', 100000.0)  # meters 
+        
     def get_termination(self, task, env, agent_id, info={}):
         """
         Terminate if the agent is too far from the waypoint.
@@ -20,7 +20,6 @@ class AgentTooFar(BaseTerminationCondition):
 
         done = bool(env.agents[agent_id].get_property_value(c.detect_agent_too_far_waypoint_state))
         if done:
-            env.agents[agent_id].crash()
             self.log(f'{agent_id} is too far from the waypoint! >{self.max_distance:.1f}m Total Steps={env.current_step}')
 
         return done, success, info    
