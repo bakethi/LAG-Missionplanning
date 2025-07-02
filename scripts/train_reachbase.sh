353ce134decd4a7eaf22c01697e26b5fa72917d0
Adrien Dimitri <adrien.maugueret@proton.me>
2025-07-02 16:51:27 +0200

    refactored reachbase config and training script

--- scripts/train_reachbase.sh
+++ scripts/train_reachbase.sh
@@ -8,10 +8,10 @@ seed=6
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, seed is ${seed}"
 CUDA_VISIBLE_DEVICES=0 python train/train_jsbsim.py \
     --env-name ${env} --algorithm-name ${algo} --scenario-name ${scenario} --experiment-name ${exp} \
-    --seed ${seed} --n-training-threads 2 --n-rollout-threads 1 --cuda \
+    --seed ${seed} --n-training-threads 2 --n-rollout-threads 8 --cuda \
     --log-interval 1 --save-interval 10 \
-    --num-mini-batch 4 --buffer-size 3000 --num-env-steps 9e3 \
+    --num-mini-batch 4 --buffer-size 3000 --num-env-steps 2e7 \
     --lr 3e-4 --gamma 0.99 --ppo-epoch 4 --clip-params 0.2 --max-grad-norm 1 --entropy-coef 0.001 \
     --hidden-size "128 128" --act-hidden-size "128 128" --recurrent-hidden-size 128 --recurrent-hidden-layers 1 --data-chunk-length 8 \
-    --render-mode "real_time" --use-eval True --eval-interval 1
-    #--user-name "adrien-maugueret" --wandb-name "waypoint-jsbsim" --use-wandb \ 
+    --user-name "adrien-maugueret" --wandb-name "waypoint-jsbsim" --use-wandb \ 
+    #--render-mode "real_time" --use-eval True --eval-interval 1
